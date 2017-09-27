888d57c544525c9861c16622f80323d7a8c95277
J Ho XRay <jho.xray@gmail.com>
2017-09-27 18:34:15 +0200

    results and shows public subscription seems to work

--- mdogs/imports/ui/public/resultsPage.jsx
+++ mdogs/imports/ui/public/resultsPage.jsx
@@ -1,10 +1,10 @@
-import { Results } from '../../api/results.js';
+//import { Results } from '../../api/results.js';
 import { Dogs } from '../../api/dogs.js';
 import { Shows } from '../../api/shows.js';
 
 import React, { Component, PropTypes } from 'react';
 import { render } from 'react-dom';
-import { createContainer } from 'meteor/react-meteor-data';
+import { withTracker } from 'meteor/react-meteor-data';
 
 class ResultLineComponent extends Component {
   constructor(props) {
@@ -18,50 +18,51 @@ class ResultLineComponent extends Component {
   }
 
   render() {
-    const dogID = this.props.item.dogID;
+    //console.log("Rendering item!");
+    //const dogID = this.props.item.dogID;
     //console.log(this.props.item);
     //console.log(dogID);
-    const dog = Dogs.findOne(dogID);
-    if (dog != undefined) {
-      mark = "";
-      if (this.props.item.mark != "--?--") mark = this.props.item.mark;
-      place = "";
-      if (this.props.item.place != "--?--") place = this.props.item.place;
-      certificate = "";
-      if (this.props.item.certificate != "--?--") certificate = this.props.item.certificate;
-      // results ugly hack
-      result = "";
-      if (this.props.item.ageResult != "--?--") result += this.props.item.ageResult;
-      if ( (this.props.item.breedResult != "--?--") && (this.props.item.breedResult.length > 0) )
-        if (result.length > 0) result += ", " + this.props.item.breedResult;
-        else result += this.props.item.breedResult;
-      if ( (this.props.item.groupResult != "--?--") && (this.props.item.groupResult.length > 0) )
-        if (result.length > 0) result += ", " + this.props.item.groupResult;
-        else result += this.props.item.groupResult;
-      if ( (this.props.item.bisResult != "--?--") && (this.props.item.bisResult.length > 0) )
-        if (result.length > 0) result += ", " + this.props.item.bisResult;
-        else result += this.props.item.bisResult;
 
-      return (
+    mark = "";
+    if (this.props.item.mark != "--?--") mark = this.props.item.mark;
+    place = "";
+    if (this.props.item.place != "--?--") place = this.props.item.place;
+    certificate = "";
+    if (this.props.item.certificate != "--?--") certificate = this.props.item.certificate;
+    // results ugly hack
+    result = "";
+    if (this.props.item.ageResult != "--?--") result += this.props.item.ageResult;
+    if ( (this.props.item.breedResult != "--?--") && (this.props.item.breedResult.length > 0) )
+      if (result.length > 0) result += ", " + this.props.item.breedResult;
+      else result += this.props.item.breedResult;
+    if ( (this.props.item.groupResult != "--?--") && (this.props.item.groupResult.length > 0) )
+      if (result.length > 0) result += ", " + this.props.item.groupResult;
+      else result += this.props.item.groupResult;
+    if ( (this.props.item.bisResult != "--?--") && (this.props.item.bisResult.length > 0) )
+      if (result.length > 0) result += ", " + this.props.item.bisResult;
+      else result += this.props.item.bisResult;
 
-        <tr onClick={this.handleClick}>
-          <td>{this.props.item.dogName}</td>
-          <td>{this.props.item.class}</td>
-          <td>{mark}</td>
-          <td>{place}</td>
-          <td>{certificate}</td>
-          <td>
-            {result}
-          </td>
-        </tr>
-      ); }
-    else return(<tr></tr>);
+    return (
+
+      <tr onClick={this.handleClick}>
+        <td>{this.props.item.dogName}</td>
+        <td>{this.props.item.class}</td>
+        <td>{mark}</td>
+        <td>{place}</td>
+        <td>{certificate}</td>
+        <td>
+          {result}
+        </td>
+      </tr>
+    );
+
   }
 }
 
 export class ResultsPublicTableView extends Component {
   constructor(props) {
       super(props);
+      //console.log("Initializing results table");
       //console.log(props);
   }
 
@@ -72,6 +73,8 @@ export class ResultsPublicTableView extends Component {
   }
 
   render() {
+    //console.log("Rendering results");
+    //console.log(this.props);
     return (
             <table className="table table-striped table-hover">
                 <thead>
@@ -93,23 +96,34 @@ export class ResultsPublicTableView extends Component {
   }
 }
 
+
+const Results = new Mongo.Collection('PublicResults');
+
 // Ok, we have to build a pretty complex thing here because need to sort results
 // first by sex, then by class
-export const ResultsPublicTable = createContainer(({ id, sex }) => {
-    const results = Results.find({showID: id}, {sort: [ ["class", "asc"], ["place", "asc"] ] }).fetch();
-    var boys = [];
-    results.forEach( function (res, index) {
-      var dog = Dogs.findOne(res.dogID);
-      //console.log(dog);
-      if (dog != undefined) {
-        res.dogName = dog.Name;
-        if (dog.sex == sex) boys.push(res);
-      }
-    });
+export const ResultsPublicTable = withTracker(({ id, sex }) => {
+    const resHandle = Meteor.subscribe('results.public', id);
+    const loading = !resHandle.ready();
 
-    return {
-        results: boys,
-        show: Shows.findOne(id)
+    if (!loading) {
+      //console.log("Sub is ready! loading...");
+      const results = Results.find({}).fetch();
+      //console.log(results);
+      var boys = [];
+      results.forEach( function (res, index) {
+        //console.log(res);
+        if (res.dogSex == sex) boys.push(res);
+      });
 
-    };
-}, ResultsPublicTableView);
+      //console.log("Results:");
+      //console.log(boys);
+      return {
+          results: boys,
+          show: Shows.findOne(id)
+
+      };
+    }
+    else return {results: []};
+
+
+}) (ResultsPublicTableView);
--- mdogs/server/main.js
+++ mdogs/server/main.js
@@ -1,6 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 
 import { Shows } from '../imports/api/shows.js';
+import { Results } from '../imports/api/results.js';
+import { Dogs } from '../imports/api/dogs.js';
+
 
 Meteor.startup(() => {
   // code to run on server at startup
@@ -11,4 +14,24 @@ Meteor.startup(() => {
 
 Meteor.publish('shows.all', function() {
   return Shows.find({});
+});
+
+// ok this is publishing a custom Collection with results combined with dog names
+Meteor.publish('results.public', function(id) {
+  const results = Results.find({showID: id}, {sort: [ ["class", "asc"], ["place", "asc"] ] }).fetch();
+
+  _self = this;
+  results.forEach( function (res, index) {
+    var dog = Dogs.findOne(res.dogID);
+    //console.log(dog);
+    if (dog != undefined) {
+      res.dogName = dog.Name;
+      res.dogSex = dog.sex;
+      _self.added('PublicResults', res._id, res);
+      //console.log("Added: ", res);
+    }
+  });
+
+  this.ready();
+  //console.log("results.public is ready");
 });
